8ed315995e4b7ea88c63205bc1600cdbff7ee9ab
Sanjeev Arya <sanjeev@ddhat.com>
2019-03-06 22:14:18 +0530

    add

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc202.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc202.net/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/oc202.net/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/oc202.net/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/oc202.net/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/oc202.net/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/oc202.net/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/oc202.net/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/oc202.net/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/oc202.net/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/oc202.net/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/oc202.net/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/oc202.net/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc202');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
