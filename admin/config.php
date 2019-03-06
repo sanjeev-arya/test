d30c0a5551da4ec607e51ce774fa38bc98a0c5aa
Sanjeev Arya <sanjeev@ddhat.com>
2019-03-06 22:27:27 +0530

    add project

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc302.net/admin/');
+define('HTTP_CATALOG', 'http://oc302.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc302.net/admin/');
+define('HTTPS_CATALOG', 'http://oc302.net/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/oc302.net/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/oc302.net/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/oc302.net/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/oc302.net/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc302');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
--- config.php
+++ config.php
@@ -1,28 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://oc202.net/');
+define('HTTP_SERVER', 'http://oc302.net/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://oc202.net/');
+define('HTTPS_SERVER', 'http://oc302.net/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/oc202.net/catalog/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/oc202.net/system/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/oc202.net/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/oc202.net/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/oc202.net/system/config/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/oc202.net/image/');
-define('DIR_CACHE', 'C:/xampp/htdocs/oc202.net/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/oc202.net/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/oc202.net/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/oc202.net/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/oc202.net/system/storage/upload/');
+define('DIR_APPLICATION', 'C:/xampp/htdocs/oc302.net/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/oc302.net/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/oc302.net/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'oc202');
+define('DB_DATABASE', 'oc302');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
